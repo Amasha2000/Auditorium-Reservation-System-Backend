db8af64bb965a589ca09ce8cd58e7ad454672032
Amasha <amashagalhenage@gmail.com>
2024-08-31 13:27:07 +0530

    Backend has initially created

---
+++ src/main/java/com/ruhuna/auditoriumreservationsystembackend/AuditoriumReservationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ruhuna.auditoriumreservationsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoriumReservationSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditoriumReservationSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruhuna/auditoriumreservationsystembackend/AuditoriumReservationSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruhuna.auditoriumreservationsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoriumReservationSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
