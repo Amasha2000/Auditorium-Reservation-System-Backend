ece5e9e4f01958ccef5520982b811ed9280df200
Amasha <amashagalhenage@gmail.com>
2024-10-12 13:57:16 +0530

    Added VC entity

---
+++ src/main/java/com/ruhuna/auditoriumreservationsystembackend/entity/VC.java
@@ -0,0 +1,41 @@
+package com.ruhuna.auditoriumreservationsystembackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+public class VC {
+}
+package com.ruhuna.reservationsystembackend.entity;
+
+        import com.ruhuna.reservationsystembackend.enums.UserRole;
+        import jakarta.persistence.*;
+        import lombok.*;
+
+        import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class VC {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vc_id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Enumerated(value = EnumType.STRING)
+    private UserRole userRole;
+
+    @OneToMany(mappedBy = "vc")
+    private List<Reservation> reservationList;
+    @OneToMany(mappedBy = "vc")
+    private List<Notification> notificationList;
+}
